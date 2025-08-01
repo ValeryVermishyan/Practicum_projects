c459bcaef683ede74f1ece3a8eb83fb1d7b4ae0a
Valery <valery.verm@yandex.ru>
2025-08-02 01:20:16 +0300

    Обновил 2 проект про агентство недвижимости

--- 2 - SQL - Агентство недвижимости/2 - Агентство недвижимости.sql
+++ 2 - SQL - Агентство недвижимости/2 - Агентство недвижимости.sql
@@ -1,35 +1,23 @@
-/* Анализ данных для агентства недвижимости
- * 
- * Автор: Вермишян Валерий
- * Дата: 03.04.2025
-*/
-
--- Фильтруем данные от аномальных значений
+-- Отфильтруем выбросы
 -- Определим аномальные значения (выбросы) по значению перцентилей:
-WITH limits AS (
-    SELECT
-        PERCENTILE_DISC(0.99) WITHIN GROUP (ORDER BY total_area) AS total_area_limit,
-        PERCENTILE_DISC(0.99) WITHIN GROUP (ORDER BY rooms) AS rooms_limit,
-        PERCENTILE_DISC(0.99) WITHIN GROUP (ORDER BY balcony) AS balcony_limit,
-        PERCENTILE_DISC(0.99) WITHIN GROUP (ORDER BY ceiling_height) AS ceiling_height_limit_h,
-        PERCENTILE_DISC(0.01) WITHIN GROUP (ORDER BY ceiling_height) AS ceiling_height_limit_l
-    FROM real_estate.flats
-),
--- Найдем id объявлений, которые не содержат выбросы:
-filtered_id AS(
-    SELECT id
-    FROM real_estate.flats  
-    WHERE 
-        total_area < (SELECT total_area_limit FROM limits)
-        AND (rooms < (SELECT rooms_limit FROM limits) OR rooms IS NULL)
-        AND (balcony < (SELECT balcony_limit FROM limits) OR balcony IS NULL)
-        AND ((ceiling_height < (SELECT ceiling_height_limit_h FROM limits)
-            AND ceiling_height > (SELECT ceiling_height_limit_l FROM limits)) OR ceiling_height IS NULL)
-    )
--- Выведем объявления без выбросов:
-SELECT *
-FROM real_estate.flats
-WHERE id IN (SELECT * FROM filtered_id);
+with limits as (
+select
+	percentile_disc(0.99) within group (order by total_area) as total_area_limit
+	,percentile_disc(0.99) within group (order by rooms) as rooms_limit
+	,percentile_disc(0.99) within group (order by balcony) as balcony_limit
+	,percentile_disc(0.99) within group (order by ceiling_height) as ceiling_height_limit_h
+	,percentile_disc(0.01) within group (order by ceiling_height) as ceiling_height_limit_l
+from real_estate.flats),
+-- Найдем объявления, которые не содержат выбросы
+filtered_id as (
+select
+	id
+from real_estate.flats
+where total_area < (select total_area_limit from limits)
+	and rooms < (select rooms_limit from limits)
+	and balcony < (select balcony_limit from limits)
+	and ((ceiling_height < (select ceiling_height_limit_h from limits)
+            and ceiling_height > (select ceiling_height_limit_l from limits)) or ceiling_height is null));
 
 -- Задача 1: Время активности объявлений
 -- Результат запроса должен ответить на такие вопросы:
@@ -39,73 +27,61 @@ WHERE id IN (SELECT * FROM filtered_id);
 --    количество комнат и балконов и другие параметры, влияют на время активности объявлений? 
 --    Как эти зависимости варьируют между регионами?
 -- 3. Есть ли различия между недвижимостью Санкт-Петербурга и Ленинградской области по полученным результатам?
--- Выведем объявления без выбросов:
-dataset AS (SELECT 
-	--Категоризировали базу по СПБ и городам ЛО
-	flats.id AS id
-	,CASE
-       WHEN city = 'Санкт-Петербург'
-            THEN '1. Санкт-Петербург'
-       ELSE '2. Города ЛО'
-    END AS region_category
-    ,CASE
-       WHEN days_exposition <= 30
-            THEN '1. До месяца'
-       WHEN days_exposition <= 90
-            THEN '2. До 3 месяцев'
-       WHEN days_exposition <= 180
-            THEN '3. До полугода'
-       ELSE '4. Более полугода'
-    END AS days_exposition_category
-	,last_price/total_area AS price_per_metr
+
+--Категоризируем базу по региону и по дням активности объявлений
+categorized_id as (
+select 
+	flats.id as id
+	,case
+		when city = 'Санкт-Петербург'
+			then '1. Санкт-Петербург'
+		else '2. Города ЛО'
+	end as region_category
+	,case
+		when days_exposition <= 30
+			then '1. До месяца'
+		when days_exposition <= 90
+			then '2. До 3 месяцев'
+		when days_exposition <= 180
+			then '3. До полугода'
+		else '4. Более полугода'
+	end as days_exposition_category
+	,last_price/total_area as price_per_metr
 	,total_area
 	,rooms
-	,COALESCE(balcony,0) AS balcony
+	,coalesce(balcony,0) as balcony
 	,floor
 	,ceiling_height
 	,floors_total
 	,living_area
 	,is_apartment
-FROM real_estate.flats
-LEFT JOIN real_estate.city USING(city_id)
-LEFT JOIN real_estate.type USING(type_id)
-LEFT JOIN real_estate.advertisement USING(id)
-WHERE id IN (SELECT * FROM filtered_id)
-	-- Оставим только города
-	AND type = 'город'
-	-- Оставим только проданные объявления, 
-	AND days_exposition IS NOT NULL),
-dataset_2 AS (SELECT
+from real_estate.flats
+left join real_estate.city using(city_id)
+left join real_estate.type using(type_id)
+left join real_estate.advertisement using(id)
+where id in (select * from filtered_id)
+	-- Оставим только города и только проданные объявления
+	and type = 'город'
+	and days_exposition is not null),
+--Сгруппируем и посчитаем статистические данные
+grouped_id as (select
 	region_category 
 	,days_exposition_category
-	,count(id) AS adds
-	,ROUND(AVG(price_per_metr)::NUMERIC,0) AS avg_price_per_metr
-	,ROUND(AVG(total_area)::NUMERIC,1) AS avg_tota_area
-	,ROUND(AVG(ceiling_height)::NUMERIC,1) AS avg_ceiling_height
-	,ROUND(AVG(floors_total)::NUMERIC,1) AS avg_floors_total
-	,ROUND(AVG(living_area)::NUMERIC,1) AS avg_living_area
-	,sum(is_apartment) AS sum_is_apartment
-	,PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY rooms) AS mediana_rooms
-	,PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY balcony) AS mediana_balcony
-	,PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY floor) AS mediana_floor
-FROM dataset
-	GROUP BY region_category, days_exposition_category)
-SELECT
-	region_category
-	,days_exposition_category
-	,adds
-	--посчитали долю категории для каждого региона
-	,ROUND((adds/sum(adds) over(PARTITION BY region_category))*100::NUMERIC,1) AS percent_adds
-	,avg_price_per_metr
-	,avg_tota_area
-	,avg_living_area
-	,avg_ceiling_height
-	,avg_floors_total
-	,sum_is_apartment
-	,mediana_rooms
-	,mediana_balcony
-	,mediana_floor
-FROM dataset_2
+	,count(id) as ads
+	--Считаем долю объявлений в каждой категории для каждого региона
+	,round((count(id) / sum(count(id)) over (partition by region_category)) * 100::numeric, 1) as percent_adds
+	,round(avg(price_per_metr)::numeric,0) as avg_price_per_metr
+	,round(avg(total_area)::numeric,1) as avg_total_area
+	,round(avg(ceiling_height)::numeric,2) as avg_ceiling_height
+	,round(avg(floors_total)::numeric,1) as avg_floors_total
+	,round(avg(living_area)::numeric,1) as avg_living_area
+	,sum(is_apartment) as total_apartments
+	,percentile_disc(0.5) within group (order by rooms) as mediana_rooms
+	,percentile_disc(0.5) within group (order by balcony) as mediana_balcony
+	,percentile_disc(0.5) within group (order by floor) as mediana_floor
+from categorized_id
+	ggroup by region_category, days_exposition_category)
+select * from grouped_id
 
 -- Задача 2: Сезонность объявлений
 -- Результат запроса должен ответить на такие вопросы:
@@ -115,59 +91,65 @@ FROM dataset_2
 --    когда происходит повышенная продажа недвижимости (по месяцам снятия объявлений)?
 -- 3. Как сезонные колебания влияют на среднюю стоимость квадратного метра и среднюю площадь квартир? 
 --    Что можно сказать о зависимости этих параметров от месяца?
--- Выведем объявления без выбросов:
-activity_adds AS (SELECT
+
+-- Выделим месяцы, когда объявление было выложено и когда снято
+activity_adds as (
+select
 	id
 	,first_day_exposition
-	,EXTRACT(MONTH FROM first_day_exposition) AS month_start
-	,(first_day_exposition + (INTERVAL '1' DAY)*days_exposition)::date AS last_day_exposition
-	,EXTRACT(MONTH FROM (first_day_exposition + (INTERVAL '1' DAY)*days_exposition)::date) AS month_stop
+	,(first_day_exposition + (interval '1' day)*days_exposition)::date as last_day_exposition
+	,extract(month from first_day_exposition) as month_start
+	,extract(month from (first_day_exposition + (interval '1' day)*days_exposition)::date) as month_stop
 	,total_area
 	,last_price/total_area AS price_per_metr
 	,days_exposition
-FROM real_estate.flats
-LEFT JOIN real_estate.city USING(city_id)
-LEFT JOIN real_estate.type USING(type_id)
-FULL JOIN real_estate.advertisement USING(id)
-WHERE id IN (SELECT * FROM filtered_id)
-	AND first_day_exposition > '2014-12-31'
-	AND first_day_exposition < '2019-01-01'
+from real_estate.flats
+left join real_estate.city using(city_id)
+left join real_estate.type using(type_id)
+full join real_estate.advertisement using(id)
+where id in (select * from filtered_id)
+	-- Оставляем объявления с 2015 по 2018 год, так как за остальной период данные неполные
+	and first_day_exposition between '2015-01-01' and '2018-12-31'
 	-- Оставим только города
-	AND type = 'город'),
+	and type = 'город'),
 -- Сгруппируем по месяцам кол-во публикаций
-count_start AS (SELECT 
-	month_start AS MONTH
-	,count(id) AS count_start
-	,ROUND(AVG(total_area)::NUMERIC,1) AS avg_total_area_start
-	,ROUND(AVG(price_per_metr)::NUMERIC,0) AS avg_price_per_metr_start
-FROM activity_adds
-GROUP BY month_start),
+count_start as (
+select
+	month_start as MONTH
+	,count(id) as count_start
+	,round(avg(total_area)::numeric,1) as avg_total_area_start
+	,round(avg(price_per_metr)::numeric,1) as avg_price_per_metr_start
+from activity_adds
+group by month_start),
 -- Сгруппируем по месяцам кол-во снятий
-count_stop AS (SELECT 
-	month_stop AS MONTH
-	,count(id) AS count_stop
-	,ROUND(AVG(total_area)::NUMERIC,1) AS avg_total_area_stop
-	,ROUND(AVG(price_per_metr)::NUMERIC,0) AS avg_price_per_metr_stop
-FROM activity_adds
-WHERE days_exposition IS NOT NULL
-GROUP BY month_stop)
-SELECT
-	MONTH
+count_stop as (
+select
+	month_stop as MONTH
+	,count(id) as count_stop
+	,round(avg(total_area)::numeric,1) as avg_total_area_stop
+	,round(avg(price_per_metr)::numeric,1) as avg_price_per_metr_stop
+from activity_adds
+-- Убираем объявления, которые еще не были сняты с продажи
+where days_exposition is not null
+group by month_stop)
+-- Посчитаем статистические данные
+select
+	month
 	,count_start
-	,rank() over(ORDER BY count_start desc) count_start_top
+	,rank() over(order by count_start desc) count_start_top
 	,avg_total_area_start
-	,rank() over(ORDER BY avg_total_area_start desc) avg_total_area_start_top
+	,rank() over(order by avg_total_area_start desc) avg_total_area_start_top
 	,avg_price_per_metr_start
-	,rank() over(ORDER BY avg_price_per_metr_start desc) avg_price_per_metr_start_top
+	,rank() over(order by avg_price_per_metr_start desc) avg_price_per_metr_start_top
 	,count_stop
-	,rank() over(ORDER BY count_stop desc) count_stop_top
+	,rank() over(order by count_stop desc) count_stop_top
 	,avg_total_area_stop
-	,rank() over(ORDER BY avg_total_area_stop desc) avg_total_area_stop_top
+	,rank() over(order by avg_total_area_stop desc) avg_total_area_stop_top
 	,avg_price_per_metr_stop
-	,rank() over(ORDER BY avg_price_per_metr_stop desc) avg_price_per_metr_stop_top
-FROM count_start
-LEFT JOIN count_stop USING (month)
-ORDER BY MONTH asc 
+	,rank() over(order by avg_price_per_metr_stop desc) avg_price_per_metr_stop_top
+from count_start
+left join count_stop using(month)
+order by month asc
 
 -- Задача 3: Анализ рынка недвижимости Ленобласти
 -- Результат запроса должен ответить на такие вопросы:
@@ -178,30 +160,25 @@ ORDER BY MONTH asc
 --    Есть ли вариация значений по этим метрикам?
 -- 4. Среди выделенных населённых пунктов какие пункты выделяются по продолжительности публикации объявлений? 
 --    То есть где недвижимость продаётся быстрее, а где — медленнее.
--- Выведем объявления без выбросов
 -- Оставим только объявления лен области и сгруппируем по населенным пунктам
-len_obl AS(SELECT
+select
 	city
-	,count(*) AS total_adds
-	,count(days_exposition) AS sold_adds
-	,round(count(days_exposition)/count(*)::NUMERIC*100,1) AS percent_sold
-	,ROUND(AVG(last_price/total_area)::NUMERIC,0) AS price_per_metr
-	,ROUND(AVG(total_area)::NUMERIC,1) AS avg_total_area
-	,PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY rooms) AS rooms_mediana
-	,PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY COALESCE(balcony,0)) AS balcony_mediana
-	,ROUND(AVG(days_exposition)::numeric,0) AS avg_days_exposition
-FROM real_estate.flats
-LEFT JOIN real_estate.city USING(city_id)
-LEFT JOIN real_estate.type USING(type_id)
-LEFT JOIN real_estate.advertisement USING(id)
-WHERE id IN (SELECT * FROM filtered_id)
+	,count(*) as total_ads
+	--если значение не null, значит объявления продано
+	,count(days_exposition) as sold_adds
+	,round(count(days_exposition)/count(*)::numeric*100,1) as per_sold
+	,round(avg(last_price/total_area)::numeric,0) as price_per_metr
+	,round(avg(total_area)::numeric,1) as avg_total_area
+	,percentile_disc(0.5) within group (order by rooms) as rooms_mediana
+	,percentile_disc(0.5) within group (order by coalesce(balcony,0)) as balcony_mediana
+	,round(avg(days_exposition)::numeric,0) as avg_days_exposition
+from real_estate.flats
+left join real_estate.city using(city_id)
+left join real_estate.type using(type_id)
+left join real_estate.advertisement using(id)
+where id in (select * from filtered_id)
 --отсекаем СПБ
-AND city <> 'Санкт-Петербург'
-GROUP BY city
-ORDER BY total_adds DESC
-LIMIT 12
-)
---Здесь проводил ранжирование для нужных полей с помощью оконных функций, чтобы ответить на все вопросы 3 задачи
-SELECT
-*
-FROM len_obl
+and city <> 'Санкт-Петербург'
+group by city
+order by total_ads desc
+limit 12
